f63128687ca40cb4c4651787bcb29c9ad7450c50
Farhan Rivaldy <fariv.fariv12@gmail.com>
2023-12-22 03:22:24 +0800

    add dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig((configEnv) => {
   return {
     plugins: [react()],
     server: {
-      port: 3000,
+      host: '0.0.0.0',
+      port: 3030,
     },
     test: {
       globals: true,
